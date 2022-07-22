c3585d129c32c8cfbb7ef0a69b98e16fe1c3d047
smodesto <sarahmodestos@gmail.com>
2022-07-21 22:00:41 -0300

    deu boa

--- cub3d/source/game/controls.c
+++ cub3d/source/game/controls.c
@@ -6,7 +6,7 @@
 /*   By: smodesto <smodesto@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 11:21:03 by smodesto          #+#    #+#             */
-/*   Updated: 2022/07/21 21:30:52 by smodesto         ###   ########.fr       */
+/*   Updated: 2022/07/21 21:57:59 by smodesto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,19 @@ void	move_forward(t_cub3d *data, t_raycasting r)
 {
 	t_point	new_pos;
 	t_point	map;
+	t_point	next_step;
 
 	new_pos = r.player.pos;
 	new_pos.x += cos(r.player.rotation_angle) * r.player.move_speed;
 	new_pos.y += sin(r.player.rotation_angle) * r.player.move_speed;
 	map.x = new_pos.x / TILE_SIZE;
 	map.y = new_pos.y / TILE_SIZE;
-	if (r.cub_map[(int)map.y][(int)map.x] != WALL)
+	next_step.x = (new_pos.x + cos(r.player.rotation_angle)
+			* r.player.move_speed) / TILE_SIZE;
+	next_step.y = (new_pos.y + sin(r.player.rotation_angle)
+			* r.player.move_speed) / TILE_SIZE;
+	if ((r.cub_map[(int)map.y][(int)map.x] != WALL)
+		&& (r.cub_map[(int)next_step.y][(int)next_step.x] != WALL))
 	{
 		data->r.player.pos = new_pos;
 		draw_game(data, data->mlx, data->img);
@@ -45,13 +51,19 @@ void	move_backward(t_cub3d *data, t_raycasting r)
 {
 	t_point	new_pos;
 	t_point	map;
+	t_point	next_step;
 
 	new_pos = r.player.pos;
 	new_pos.x -= cos(r.player.rotation_angle) * r.player.move_speed;
 	new_pos.y -= sin(r.player.rotation_angle) * r.player.move_speed;
 	map.x = new_pos.x / TILE_SIZE;
 	map.y = new_pos.y / TILE_SIZE;
-	if (r.cub_map[(int)map.y][(int)map.x] != WALL)
+	next_step.x = (new_pos.x - cos(r.player.rotation_angle)
+			* r.player.move_speed) / TILE_SIZE;
+	next_step.y = (new_pos.y - sin(r.player.rotation_angle)
+			* r.player.move_speed) / TILE_SIZE;
+	if ((r.cub_map[(int)map.y][(int)map.x] != WALL)
+		&& (r.cub_map[(int)next_step.y][(int)next_step.x] != WALL))
 	{
 		data->r.player.pos = new_pos;
 		draw_game(data, data->mlx, data->img);
