f8cf373f9cb76c415a2af4ac2a89de9345f33c38
smodesto <sarahmodestos@gmail.com>
2022-08-18 20:53:57 -0300

    WASD finally

--- cub3d/source/game/controls.c
+++ cub3d/source/game/controls.c
@@ -6,7 +6,7 @@
 /*   By: smodesto <smodesto@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 11:21:03 by smodesto          #+#    #+#             */
-/*   Updated: 2022/08/17 23:13:55 by smodesto         ###   ########.fr       */
+/*   Updated: 2022/08/18 20:51:10 by smodesto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,17 +64,14 @@ void	move_left(t_cub3d *data, t_raycasting r, int key)
 	t_point	map;
 	t_point	next_step;
 
-	if ((!is_ray_facing_left(r.ray_angle) && !is_ray_facing_right(r.ray_angle))
-		&& (key == MAIN_KEY_A))
-		return (move_right(data, r, key));
 	new_pos = r.player.pos;
-	new_pos.x -= sin(r.player.rotation_angle) * r.player.move_speed;
-	new_pos.y -= cos(r.player.rotation_angle) * r.player.move_speed;
+	new_pos.x -= sin(-r.player.rotation_angle) * r.player.move_speed;
+	new_pos.y -= cos(-r.player.rotation_angle) * r.player.move_speed;
 	map.x = new_pos.x / TILE_SIZE;
 	map.y = new_pos.y / TILE_SIZE;
-	next_step.x = (new_pos.x - sin(r.player.rotation_angle)
+	next_step.x = (new_pos.x - sin(-r.player.rotation_angle)
 			* r.player.move_speed) / TILE_SIZE;
-	next_step.y = (new_pos.y - cos(r.player.rotation_angle)
+	next_step.y = (new_pos.y - cos(-r.player.rotation_angle)
 			* r.player.move_speed) / TILE_SIZE;
 	if ((r.cub_map[(int)map.y][(int)map.x] != WALL)
 		&& (r.cub_map[(int)next_step.y][(int)next_step.x] != WALL))
@@ -90,17 +87,14 @@ void	move_right(t_cub3d *data, t_raycasting r, int key)
 	t_point	map;
 	t_point	next_step;
 
-	if ((!is_ray_facing_left(r.ray_angle) && !is_ray_facing_right(r.ray_angle))
-		&& (key == MAIN_KEY_D))
-		return (move_left(data, r, key));
 	new_pos = r.player.pos;
-	new_pos.x += sin(r.player.rotation_angle) * r.player.move_speed;
-	new_pos.y += cos(r.player.rotation_angle) * r.player.move_speed;
+	new_pos.x += sin(-r.player.rotation_angle) * r.player.move_speed;
+	new_pos.y += cos(-r.player.rotation_angle) * r.player.move_speed;
 	map.x = new_pos.x / TILE_SIZE;
 	map.y = new_pos.y / TILE_SIZE;
-	next_step.x = (new_pos.x + sin(r.player.rotation_angle)
+	next_step.x = (new_pos.x + sin(-r.player.rotation_angle)
 			* r.player.move_speed) / TILE_SIZE;
-	next_step.y = (new_pos.y + cos(r.player.rotation_angle)
+	next_step.y = (new_pos.y + cos(-r.player.rotation_angle)
 			* r.player.move_speed) / TILE_SIZE;
 	if ((r.cub_map[(int)map.y][(int)map.x] != WALL)
 		&& (r.cub_map[(int)next_step.y][(int)next_step.x] != WALL))
